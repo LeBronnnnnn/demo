256b1584a76222ba53c33621d63ee0c7bea5549b
LeBronnnnnn <15841125023@126.com>
2023-11-03 16:45:22 +0800

    auto

--- test.cpp
+++ test.cpp
@@ -1,32 +1,8 @@
-#include<>
-//快排   
-//生成随机数  
-#include <iostream>
-void creatRandomNumber (int *a, int n) {
-    srand((unsigned)time(NULL));
-    for (int i = 0; i < n; i++) {
-        a[i] = rand() % 100;
-    }
-}
-//快速排序  
-void quickSort(int *a, int left, int right) {
-    if (left >= right) {
-        return;
-    }
-    int i = left;
-    int j = right;
-    int key = a[left];
-    while (i < j) {
-        while (i < j && key <= a[j]) {
-            j--;
-        }
-        a[i] = a[j];
-        while (i < j && key >= a[i]) {
-            i++;
-        }
-        a[j] = a[i];
-    }
-    a[i] = key;
-    quickSort(a, left, i - 1);
-    quickSort(a, i + 1, right);
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World"<<endl;
+    auto a = "sss";
+    cout<<a<<endl;
+    return 0;
 }
